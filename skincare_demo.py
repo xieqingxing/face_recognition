6766e9f4eff71d393f36d6b89ad539304b8020c8
xieqingxing <2839612300@qq.com>
2019-04-09 10:33:32 +0800

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: xieqingxing <2839612300@qq.com>
    
    # Conflicts:
    #	README.md

--- skincare_demo.py
+++ skincare_demo.py
@@ -13,16 +13,24 @@ def white_face(image):
                     dst[i, j][c] = 255
                 elif color < 0:  # 防止像素值越界（0~255）
                     dst[i, j][c] = 0
-    cv2.waitKey(0)
-    cv2.imwrite('test_1.jpg', image)
     return dst
 
+start = cv2.getTickCount()
 image = cv2.imread('./skin_test.jpg')
 image = white_face(image)
 # 高斯模糊
-cv2.GaussianBlur(image, (9, 9), 0, 0)
-# cv2.imshow('test.jpg',image)
-bilateralFilterVal = 30
+image = cv2.GaussianBlur(image, (5,5), 0, 0)
+# 滤波
+image = cv2.bilateralFilter(image,30,60,15)
+# 图像增强
+image2 = cv2.GaussianBlur(image,(0,0),9)
+res = cv2.addWeighted(image,1.5,image2,-0.5,0)
+end = cv2.getTickCount()
+during = (end - start) / cv2.getTickFrequency()
+print(during)
+cv2.imshow('test.jpg',res)
+cv2.imwrite('test_4.jpg',res)
+cv2.waitKey(0)
 
 
 
